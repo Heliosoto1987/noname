095eae50cd3914f142df079fc8357bfda5b78cee
Helio <heliosoto17@gmail.com>
2021-09-14 02:52:38 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { LandingPage } from "./components/Screen/LandingPage";
+
+export const App = () => {
+  return <LandingPage />;
+};
---
+++ src/components/Screen/LandingPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../styles/landing.css";
+
+export const LandingPage = () => {
+  return <div className="landing">Hola</div>;
+};
