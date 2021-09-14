39a23ad9094f6d77f8dcba751d32188000f30369
Helio <heliosoto17@gmail.com>
2021-09-14 03:42:51 +0200

    Updates

---
+++ static/js/main.523eeea0.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpnoname=this.webpackJsonpnoname||[]).push([[0],{10:function(n,e,t){},11:function(n,e,t){"use strict";t.r(e);t(1);var c=t(3),i=t.n(c),o=(t(8),t(0)),s=function(){return Object(o.jsx)("div",{className:"landing",children:Object(o.jsx)("h2",{className:"landing__title",children:"Coming Soon"})})},a=function(){return Object(o.jsx)(s,{})};t(10);i.a.render(Object(o.jsx)(a,{}),document.getElementById("root"))},8:function(n,e,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.523eeea0.chunk.js.map
